b5d4f4348d18f15b41f5a8c0142e8fb24131f413
Bram Droppers <bram.droppers@gmail.com>
2024-03-26 13:10:26 +0100

    fixed runner

--- run_continuous-integration.sh
+++ run_continuous-integration.sh
@@ -3,8 +3,8 @@
 ## Assert simulation
 reference_dir=./simulation/reference_output/main_20240326/RhineMeuse_30min_natural/netcdf/
 comparison_dir=./simulation/output/RhineMeuse_30min_natural/netcdf/
-python ./assert_simulation_netcdf.txt $reference_dir $comparison_dir
+python ./assert_simulation_netcdf.py $reference_dir $comparison_dir
 
 reference_dir=./simulation/reference_output/main_20240326/RhineMeuse_30min_natural/states/
 comparison_dir=./simulation/output/RhineMeuse_30min_natural/states/
-python ./assert_simulation_states.txt $reference_dir $comparison_dir
+python ./assert_simulation_states.py $reference_dir $comparison_dir
